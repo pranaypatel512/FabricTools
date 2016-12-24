db832eac881a73252415813c64793941c950d8fe
pranaypatel512 <www.pranay1337.com@gmail.com>
2016-12-25 02:59:36 +0530

    implement: [*] add user info when app reported a crash

--- Crashlytics/app/src/main/java/com/pranay/fabrictools/crashlytics/MainActivity.java
+++ Crashlytics/app/src/main/java/com/pranay/fabrictools/crashlytics/MainActivity.java
@@ -14,10 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
+        // TODO: Move this to where you establish a user session
+        logUser();
+
     }
 
     public void forceCrash(View view) {
         int answer=111/0; // this line throw exception
+    }
+    private void logUser() {
+        // TODO: Use the current user's information
+        // You can call any combination of these three methods
+        Crashlytics.setUserIdentifier("12345");
+        Crashlytics.setUserEmail("user@fabric.io");
+        Crashlytics.setUserName("Test User");
     }
 
 }
